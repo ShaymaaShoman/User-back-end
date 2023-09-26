8c0b716bfae59cfec27c0a19da38776b594b009f
Shaymaa Shoman <shemooshoman@gmial.com>
2023-09-26 19:01:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');//import express
+const app = express()//app allow to declare end point
+
+app.use(express.json())
+const users=[]
+//HTTP METHOD
+//GET -Get Data
+app.get('/',(request,response)=>{
+    response.send("Welcome Home")
+})
+app.get('/users',(request,response)=>{
+    if(users.length==0){
+        response.status(404).send("No users found!")
+        return
+    }
+    response.status(200).send(users)
+})
+
+//POST Create-data
+app.post('/users',(request,response)=>{
+    // console.log(request.body)
+    const user =request.body;
+    const findUser= users.find((x)=>x.id===user.id)
+    if(findUser){
+        response.status(400).send("User already exist")
+        return
+    }
+    users.push(user)
+    response.status(201).send("Create!")
+})
+//PUT
+//DELETE
+app.delete('/users/:id',(request,response)=>{
+const {id}=request.params
+const findUserIndex =users.findIndex((x)=>x.id===id)
+if(findUserIndex == -1){
+response.status(400).send("User not found")
+return
+}
+users.slice(findUserIndex,1)
+response.status(200).send("User Delete Sucsess")
+})
+
+app.listen(3000,()=>{ //import make port in browser
+    console.log("Done")
+})
+
+//to run application /*node app.js*/
